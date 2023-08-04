b6e414701d24a843d54469bd6bdeaf6a541fd38d
Daniel Bravo <danielbravo1202@gmail.com>
2023-08-04 12:02:02 -0500

    fix: unused imports

--- src/main/java/com/soap/SpringBootSoapApplication.java
+++ src/main/java/com/soap/SpringBootSoapApplication.java
@@ -1,13 +1,7 @@
 package com.soap;
 
-import com.soap.client.SoapClient;
-import com.soap.wsdl.AddResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootSoapApplication {
--- src/main/java/com/soap/wsdl/package-info.java
+++ src/main/java/com/soap/wsdl/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por Eclipse Implementation of JAXB v2.3.6 
 // Visite https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2023.08.02 a las 11:19:27 PM PET 
+// Generado el: 2023.08.04 a las 12:00:59 PM PET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://tempuri.org/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
